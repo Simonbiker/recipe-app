cc9bce7a2da0dc249057e07b3011bd4f79adc39a
Simonbiker <smacr25@gmail.com>
2019-06-16 15:51:09 +0100

    Adding Nav link for recipe & shopping list. Wiring all that up. Passing recipe data with property binding. Passing data with event binding. FormsModule added. Allowing the user to add ingredients to shopping list

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Recipe } from '../receipe.model';
 
 @Component({
@@ -7,14 +7,20 @@ import { Recipe } from '../receipe.model';
   styleUrls: ['./recipe-list.component.scss']
 })
 export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg'),
-    new Recipe('A test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg')
+    // tslint:disable-next-line:max-line-length
+    new Recipe('Another test recipe', 'This is simply a test', 'https://cdn.pixabay.com/photo/2017/05/07/08/56/pancakes-2291908_960_720.jpg')
   ];
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 
 }
